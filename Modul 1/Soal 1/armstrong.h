1fee9728c382d905f6be9d8d6109b632d087a389
Araryarch <araryaarch@gmail.com>
2024-09-13 16:21:21 +0700

    one liner

--- Modul 1/Soal 1/armstrong.h
+++ Modul 1/Soal 1/armstrong.h
@@ -3,12 +3,5 @@
 
 void armstrong(int num)
 {
-  if ((pow(num / 100, 3) + pow(num % 100 / 10, 3) + pow(num % 10, 3)) == num && num > 99)
-  {
-    printf("Bilangan Armstrong");
-  }
-  else
-  {
-    printf("Bukan Bilangan Armstrong");
-  }
+  ((pow(num / 100, 3) + pow(num % 100 / 10, 3) + pow(num % 10, 3)) == num && num > 99) ? printf("Bilangan Armstrong") : printf("Bukan Bilangan Armstrong");
 }
