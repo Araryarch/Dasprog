35fcd292d4e6f132b54d5ecaec4372e0cc4d9e2e
Araryarch <araryaarch@gmail.com>
2024-09-13 16:29:03 +0700

    update armstrong.h

--- Modul 1/Soal 1/armstrong.h
+++ Modul 1/Soal 1/armstrong.h
@@ -3,5 +3,5 @@
 
 void armstrong(int num)
 {
-  ((pow(num / 100, 3) + pow(num % 100 / 10, 3) + pow(num % 10, 3)) == num && num > 99) ? printf("Bilangan Armstrong") : printf("Bukan Bilangan Armstrong");
+  ((pow(num / 100, 3) + pow(num % 100 / 10, 3) + pow(num % 10, 3)) == num && num > 99) ? printf("Merupakan Bilangan Armstrong") : printf("Bukan Bilangan Armstrong");
 }
